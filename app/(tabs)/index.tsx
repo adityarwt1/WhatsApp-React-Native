c6b2a7cc0200dc4c521aafa66deff6d4d1b20910
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-10-19 13:05:08 +0530

    done the logo part

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,46 @@
+import { Entypo } from "@expo/vector-icons";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-const index = () => {
+const Index = () => {
   return (
-    <>
-      <View style={styles.constainer}>
+    <View style={styles.container}>
+      {/* Header */}
+      <View style={styles.header}>
+        <Text style={styles.headerText}>WhatsApp</Text>
+        <Entypo name="dots-three-vertical" size={22} color="white" />
+      </View>
+
+      {/* Body */}
+      <View style={styles.body}>
         <Text style={styles.text}>index</Text>
       </View>
-    </>
+    </View>
   );
 };
 
-export default index;
+export default Index;
 
 const styles = StyleSheet.create({
-  constainer: {
+  container: {
+    flex: 1,
     backgroundColor: "#0B1014",
+    paddingTop: 50, // keeps it away from notch
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    backgroundColor: "#0B1014",
+  },
+  headerText: {
+    color: "white",
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  body: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
